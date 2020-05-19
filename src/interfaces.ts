bd5cabee472bccf7ae3e317f080e24a50ccf7473
Lukas Valenta <lukas.valenta@applifting.cz>
2020-05-19 11:16:53 +0200

    feat: Basic interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,15 @@
+import { MappingAST, ParsedProfile, Result } from 'superface';
+
+import { MapValidationError } from './errors';
+
+export interface ProfileParser {
+  parse(profileId: string): ParsedProfile;
+}
+
+export interface MappingParser {
+  parse(map: string): MappingAST;
+}
+
+export interface MapValidator {
+  validate(map: string): Result<string, MapValidationError>;
+}
