d8701bed7200ff7fef9f0cc7f4f99742c8f85370
Lukas Valenta <lukas.valenta@applifting.cz>
2021-01-12 12:56:24 +0100

    chore: Export getProfileOutput function

---
+++ src/interpreter/utils.ts
@@ -0,0 +1,11 @@
+import { ProfileDocumentNode } from '@superfaceai/ast';
+import { ProfileIOAnalyzer } from './profile-io-analyzer';
+import { ProfileOutput } from './profile-output';
+
+export const getProfileOutput = (
+  profile: ProfileDocumentNode
+): ProfileOutput => {
+  const analyzer = new ProfileIOAnalyzer();
+
+  return analyzer.visit(profile);
+};
