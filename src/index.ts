7f71dfb06cb14028e228b69b34449205cac73668
Eduard Lavuš <eduard.lavus@applifting.cz>
2021-02-22 12:19:25 +0100

    feat: export VERSION constant

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import PJ from '../package.json';
+
 export * from './errors';
 export * from './language';
 export * from './interpreter';
 export * from './common';
+
+export const VERSION = PJ.version;
