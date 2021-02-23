ac967bd0779c301f9192c2303849bb9af28ac8cb
Eduard Lavuš <eduard.lavus@applifting.cz>
2021-02-23 15:39:11 +0100

    fix: use require and use set correct rootDir

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import packageJson from '../package.json';
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-var-requires
+const packageJson = require('../package.json');
 
 export * from './errors';
 export * from './language';
 export * from './interpreter';
 export * from './common';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
 export const VERSION = packageJson.version;
