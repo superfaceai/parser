0deed0f9943a9d63f31b1af6c767f6dfcb1a831b
Jakub Vacek <jakub.vacek@dxheroes.io>
2021-04-06 13:51:37 +0200

    test: add constructDebugVisualTree tests

--- src/language/jessie/validator/validator.test.ts
+++ src/language/jessie/validator/validator.test.ts
@@ -63,60 +63,61 @@ expect.extend({
   },
 });
 
-describe('ScriptValidator', () => {
-  test.each([
-    ['empty statement', ';', ['EmptyStatement construct is not supported']],
-    [
-      'non-strict equality',
-      '1 == 1 && 1 != 2',
-      ['Use `1 === 1` instead', 'Use `1 !== 2` instead'],
-    ],
-    [
-      'prefix ++ and --',
-      `
+describe('validator', () => {
+  describe('validateScript', () => {
+    test.each([
+      ['empty statement', ';', ['EmptyStatement construct is not supported']],
+      [
+        'non-strict equality',
+        '1 == 1 && 1 != 2',
+        ['Use `1 === 1` instead', 'Use `1 !== 2` instead'],
+      ],
+      [
+        'prefix ++ and --',
+        `
         let x = 1;
         ++x;
         --x;
         `,
-      ['Use `x += 1` or `x++` instead', 'Use `x -= 1` or `x--` instead'],
-    ],
-    [
-      'function declarations',
-      'function foo(a, b) {}',
-      ['Use `const foo = (a, b) => { /* body */ }` instead'],
-    ],
-    [
-      'var keyword',
-      'var x = 45',
-      ['Use `const x = 45` or `let x = 45` instead'],
-    ],
-    [
-      'import and export',
-      `
+        ['Use `x += 1` or `x++` instead', 'Use `x -= 1` or `x--` instead'],
+      ],
+      [
+        'function declarations',
+        'function foo(a, b) {}',
+        ['Use `const foo = (a, b) => { /* body */ }` instead'],
+      ],
+      [
+        'var keyword',
+        'var x = 45',
+        ['Use `const x = 45` or `let x = 45` instead'],
+      ],
+      [
+        'import and export',
+        `
         import "foo";
         export {};
         `,
-      [
-        'ImportDeclaration construct is not supported',
-        'ExportDeclaration construct is not supported',
+        [
+          'ImportDeclaration construct is not supported',
+          'ExportDeclaration construct is not supported',
+        ],
       ],
-    ],
-    [
-      'exceptions',
-      `
+      [
+        'exceptions',
+        `
         try {
           throw "exception";
         } catch (ex) {} finally {}
         `,
-      [
-        'TryStatement construct is not supported',
-        'ThrowStatement construct is not supported',
-        'CatchClause construct is not supported',
+        [
+          'TryStatement construct is not supported',
+          'ThrowStatement construct is not supported',
+          'CatchClause construct is not supported',
+        ],
       ],
-    ],
-    [
-      'classes, instantiation and this',
-      `
+      [
+        'classes, instantiation and this',
+        `
         class Foo {
           constructor() {
             this.foo = 1;
@@ -124,41 +125,41 @@ describe('ScriptValidator', () => {
         }
         let x = new Foo();
         `,
-      [
-        'ClassDeclaration construct is not supported',
-        'Constructor construct is not supported',
-        'ThisKeyword construct is not supported',
-        'NewExpression construct is not supported',
+        [
+          'ClassDeclaration construct is not supported',
+          'Constructor construct is not supported',
+          'ThisKeyword construct is not supported',
+          'NewExpression construct is not supported',
+        ],
       ],
-    ],
-    [
-      'for/in statement',
-      `
+      [
+        'for/in statement',
+        `
         const o = { a: 1, b: 2, c: 3 }
         for (let key in o) {}
         `,
-      ['ForInStatement construct is not supported'],
-    ],
-    [
-      'with statement',
-      `
+        ['ForInStatement construct is not supported'],
+      ],
+      [
+        'with statement',
+        `
         const a = { a: 1, b: 2, c: 3 }
         with (a) {}
         `,
-      ['WithStatement construct is not supported'],
-    ],
-  ])('ForbidenConstructs: %s', (_name, script: string, errors: string[]) => {
-    expect(script).not.toBeValidScript(...errors);
-  });
+        ['WithStatement construct is not supported'],
+      ],
+    ])('ForbidenConstructs: %s', (_name, script: string, errors: string[]) => {
+      expect(script).not.toBeValidScript(...errors);
+    });
 
-  test.each([
-    [
-      'JSON',
-      '{ "a": 1, "b": true, "c": "hi", "d": null, "e": [1, "a"], "f": { "a": -1 } }',
-    ],
-    [
-      'const, let and assignment operators',
-      `
+    test.each([
+      [
+        'JSON',
+        '{ "a": 1, "b": true, "c": "hi", "d": null, "e": [1, "a"], "f": { "a": -1 } }',
+      ],
+      [
+        'const, let and assignment operators',
+        `
         const x = 41;
         let y = 43;
         y += 2;
@@ -166,51 +167,51 @@ describe('ScriptValidator', () => {
         y *= 2;
         y /= 2;
         `,
-    ],
-    ['arithmetic', '-1 + (+2) - 3 / 4 * 5 % 6 ** 2'],
-    [
-      'postfix ++ and --',
-      `
+      ],
+      ['arithmetic', '-1 + (+2) - 3 / 4 * 5 % 6 ** 2'],
+      [
+        'postfix ++ and --',
+        `
         let x = 0;
         x++;
         x--;
         `,
-    ],
-    ['bitwise', '~1 & 2 | 3 ^ 4 >> 1 << 2 >>> 3'],
-    [
-      'comparison (but only strict equality)',
-      '1 < 2 <= 3 > 4 >= 5 === 6 !== 7',
-    ],
-    [
-      'destructuring',
-      `
+      ],
+      ['bitwise', '~1 & 2 | 3 ^ 4 >> 1 << 2 >>> 3'],
+      [
+        'comparison (but only strict equality)',
+        '1 < 2 <= 3 > 4 >= 5 === 6 !== 7',
+      ],
+      [
+        'destructuring',
+        `
         const { x, y, z } = { x: 1, y: 2, z: "foo" }
         const [a, b, c] = [1, 2, "foo"]
         `,
-    ],
-    [
-      'spreading',
-      `
+      ],
+      [
+        'spreading',
+        `
         const x = { "a": 1, "b": 2 }
         const y = { ...x, "a": 3 }
         const a = [1, 2, 3]
         const b = [...a, 4, 5]
         `,
-    ],
-    [
-      'arrow functions, calls and returns',
-      `
+      ],
+      [
+        'arrow functions, calls and returns',
+        `
         const foo = (x) => {
           if (x === 0) return 0;
           return x + foo(x - 1],
         }
         `,
-    ],
-    ['rest parameter', '(x, ...rest) => rest.length'],
-    ['template strings', '(x, y) => `${x} and ${y}`'],
-    [
-      'loops',
-      `
+      ],
+      ['rest parameter', '(x, ...rest) => rest.length'],
+      ['template strings', '(x, y) => `${x} and ${y}`'],
+      [
+        'loops',
+        `
         const a = [1, 2, 3]
         
         for (let x of a) {}
@@ -225,10 +226,10 @@ describe('ScriptValidator', () => {
           continue;
         } while (false)
         `,
-    ],
-    [
-      'conditionals',
-      `
+      ],
+      [
+        'conditionals',
+        `
         if (true && true) {} else {}
         switch (1) {
           case 2:
@@ -240,8 +241,49 @@ describe('ScriptValidator', () => {
         }
         let x = (true || false) ? 1 : 2
         `,
-    ],
-  ])('Valid scripts: %s', (_name, script) => {
-    expect(script).toBeValidScript();
+      ],
+    ])('Valid scripts: %s', (_name, script) => {
+      expect(script).toBeValidScript();
+    });
+  });
+  describe('constructDebugVisualTree', () => {
+    let consoleDebugSpy: jest.SpyInstance;
+    const originalLogLevel = process.env.LOG_LEVEL;
+
+    beforeAll(() => {
+      process.env.LOG_LEVEL = 'debug';
+    });
+
+    afterAll(() => {
+      process.env.LOG_LEVEL = originalLogLevel;
+    });
+
+    beforeEach(() => {
+      consoleDebugSpy = jest
+        .spyOn(console, 'debug')
+        .mockImplementation(() => undefined);
+    });
+
+    afterEach(() => {
+      jest.resetAllMocks();
+    });
+
+    it('constructs debug tree from empty statement', () => {
+      validateScript(';');
+
+      expect(consoleDebugSpy).toHaveBeenCalledTimes(1);
+      expect(consoleDebugSpy).toHaveBeenCalledWith(
+        'NODE SourceFile ";"\n\tNODE EmptyStatement ";" [S]\n\tNODE EndOfFileToken ""\n'
+      );
+    });
+
+    it('constructs debug tree from non strict equality', () => {
+      validateScript('1 == 1 && 1 != 2');
+
+      expect(consoleDebugSpy).toHaveBeenCalledTimes(1);
+      expect(consoleDebugSpy).toHaveBeenCalledWith(
+        'NODE SourceFile "1 == 1 && 1 != 2"\n\tNODE ExpressionStatement "1 == 1 && 1 != 2"\n\t\tNODE BinaryExpression "1 == 1 && 1 != 2"\n\t\t\tNODE BinaryExpression "1 == 1"\n\t\t\t\tNODE FirstLiteralToken "1"\n\t\t\t\tNODE EqualsEqualsToken "==" [R]\n\t\t\t\tNODE FirstLiteralToken "1"\n\t\t\tNODE AmpersandAmpersandToken "&&"\n\t\t\tNODE BinaryExpression "1 != 2"\n\t\t\t\tNODE FirstLiteralToken "1"\n\t\t\t\tNODE ExclamationEqualsToken "!=" [R]\n\t\t\t\tNODE FirstLiteralToken "2"\n\tNODE EndOfFileToken ""\n'
+      );
+    });
   });
 });
