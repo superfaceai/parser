9db9ae250cec048eff85d22f0eab31c0ec7a2f29
Martin Albert <martin.albert.187@gmail.com>
2021-08-25 12:00:39 +0200

    fix: do not visit variable if undefined

--- src/interpreter/map-validator.ts
+++ src/interpreter/map-validator.ts
@@ -551,8 +551,11 @@ export class MapValidator implements MapAstVisitor {
       const variable = this.variables[variableName];
 
       this.currentStructure = type;
-      result = this.visit(variable);
 
+      if (variable !== undefined) {
+        result = this.visit(variable);
+      }
+      
       if (!result) {
         this.addIssue({
           kind: 'wrongVariableStructure',
