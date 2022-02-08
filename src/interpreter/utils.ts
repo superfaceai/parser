7117f5fc8ffee43441dd272f00081c3d559a41ee
Jakub Vacek <vacjak@seznam.cz>
2022-02-08 15:07:35 +0100

    chore: clear validation issue message

--- src/interpreter/utils.ts
+++ src/interpreter/utils.ts
@@ -125,7 +125,7 @@ export function formatIssueContext(issue: ValidationIssue): string {
       return `Wrong Profile Name: expected "${issue.context.expected}", but got "${issue.context.actual}"`;
 
     case 'wrongProfileVersion':
-      return `Wrong Profile Version: expected "${composeVersion(
+      return `Version is not matching: expected "${composeVersion(
         issue.context.expected
       )}", but map requests "${composeVersion(issue.context.actual)}"`;
 
